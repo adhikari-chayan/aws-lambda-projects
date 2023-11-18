32e1343b198c0b870c00b9366a037440991ce066
Chayan Adhikari <adhikari.chayan@gmail.com>
2023-11-18 13:32:30 +0530

    kinesis sample

---
+++ kinesis-course/functions/consumer.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports.handler = async (event) => {
+  const records = event.Records;
+
+  for (const record of records) {
+      const data = record.kinesis.data;
+      const decodedData = new Buffer.from(data, 'base64').toString('utf-8');
+      // const parsedData = JSON.parse(decodedData);
+      console.log(decodedData);
+  }
+  return {
+      statusCode: 200,
+      body: 'Success',
+  };
+};
---
+++ kinesis-course/producer.js
@@ -0,0 +1,32 @@
+const AWS = require("aws-sdk");
+
+const kinesis = new AWS.Kinesis({
+  region: "us-east-1",
+});
+
+const streamName = "KP1-dev-KP1DataStream-nF6X16zBT3li";
+
+function generateTelemetry() {
+  setInterval(() => {
+    const temperature = 20 + Math.random() * 10;
+    const wind = Math.random() * 10;
+    const pressure = 980 + Math.random() * 40;
+    const telemetry = { temperature, wind, pressure };
+
+    const params = {
+      Data: JSON.stringify(telemetry),
+      PartitionKey: "1234",
+      StreamName: streamName,
+    };
+
+    kinesis.putRecord(params, (err, data) => {
+      if (err) {
+        console.error(err, err.stack);
+      } else {
+        console.log(data);
+      }
+    });
+  }, 500);
+}
+
+generateTelemetry();
